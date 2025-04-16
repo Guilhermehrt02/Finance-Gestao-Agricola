3b0127b50e9ebd605dfe02e0afa8f07d7cea0676
Guilhermehrt02 <ghorita.gh@gmail.com>
2025-04-15 21:49:04 -0300

    feat(finance): enhance report chart options

--- HandsOn/libs/shared/ui/src/index.ts
+++ HandsOn/libs/shared/ui/src/index.ts
@@ -21,3 +21,5 @@ export * from './lib/modules/expense-form/expense-form.component';
 export * from './lib/modules/revenue-form/revenue-form.component';
 export * from './lib/components/chart/chart.component';
 export * from './lib/modules/report-chart/report-chart.component';
+
+export * from './lib/modules/report-bar/report-bar.component';
--- HandsOn/libs/shared/ui/src/lib/modules/report-chart/report-chart.component.ts
+++ HandsOn/libs/shared/ui/src/lib/modules/report-chart/report-chart.component.ts
@@ -1,40 +1,76 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartModule } from 'primeng/chart';
-import { ChartData, ChartOptions } from 'chart.js';
+import {
+  ChartData,
+  ChartOptions,
+  ChartType,
+  registerables,
+  Chart,
+} from 'chart.js';
 
 @Component({
   selector: 'lib-report-chart',
+  standalone: true,
   imports: [CommonModule, ChartModule],
   templateUrl: './report-chart.component.html',
   styleUrl: './report-chart.component.css',
 })
-export class ReportChartComponent implements OnInit{ 
-  @Input() data: ChartData<'pie', number[], unknown> | null = null;
+export class ReportChartComponent implements OnInit {
+  @Input() data: ChartData | null = null;
   @Input() options: ChartOptions = {};
-  @Input() type: 'pie' | 'doughnut' | 'bar' | 'line' | 'polarArea' | 'radar' = 'pie';
+  @Input() type: ChartType = 'pie';
   @Input() label = '';
   @Input() loading = false;
   @Input() className = 'w-full max-w-xl h-80 mx-auto';
 
   ngOnInit(): void {
-    if (this.type === 'pie' || this.type === 'doughnut') {
-      this.options = {
-        ...this.options,
-        plugins: {
-          ...this.options.plugins,
-          legend: {
-            position: 'bottom',
-            labels: {
-              color: '#374151', 
-            },
-          },
-        },
-        scales: {
-          x: { display: false },
-          y: { display: false },
-        },
-      };
+    Chart.register(...registerables); 
+
+    this.options = this.getChartOptions(this.type);
+  }
+
+  private getChartOptions(type: ChartType): ChartOptions {
+    const commonLegend = {
+      position: 'bottom' as const,
+      labels: {
+        color: '#374151',
+      },
+    };
+
+    const commonScales = {
+      x: {
+        ticks: { color: '#374151' },
+        grid: { color: '#E5E7EB' },
+      },
+      y: {
+        ticks: { color: '#374151' },
+        grid: { color: '#E5E7EB' },
+      },
+    };
+
+    switch (type) {
+      case 'pie':
+      case 'doughnut':
+      case 'polarArea':
+        return {
+          plugins: { legend: commonLegend },
+          responsive: true,
+        };
+
+      case 'bar':
+      case 'line':
+      case 'radar':
+        return {
+          responsive: true,
+          plugins: { legend: commonLegend },
+          scales: commonScales,
+        };
+
+      default:
+        return {
+          responsive: true,
+        };
     }
   }
 }
